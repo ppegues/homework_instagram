b700d05bd5da92b32b901134bb3fc00cb4fd7734
ppegues <pacepegues@Pace-Peguess-MacBook-Pro.local>
2014-11-10 14:11:46 -0600

    Finished the "R"

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,9 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
-
+  def show
+   photo= Photo.find_by({:id => params[:id]})
+   @picture = photo.source
+   @caption = photo.caption
+ end
 end
