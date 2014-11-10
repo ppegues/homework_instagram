c847c502e601bdf646b3a9b0c662d96dfc0d5a79
ppegues <pacepegues@Pace-Peguess-MacBook-Pro.local>
2014-11-10 15:56:17 -0600

    Finished, but still issues
    I have all the links working, but the pictures are not updating and
    deleting when I go back and refresh “…/all_photos”

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,7 +17,17 @@
     photo.save
   end
   def destroy
-    photo = Photo.find(:id)
+    photo = Photo.find{:id}
     photo.destroy
+  end
+  def edit_form
+    @photo = Photo.find_by({:id => params[:id]})
+    @photo.caption
+    @picture = @photo.source
+  end
+  def update_row
+    photo = Photo.find{:id}
+    photo.caption ="the_caption"
+    photo.save
   end
   end
